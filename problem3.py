511f4f5cde2c8c8beecb8a1f1b3cfa6a83946821
Condrat Mihai <condratmihai0@gmail.com>
2019-11-14 22:19:50 +0200

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,13 +1,13 @@
 from problem import Problem
 import random
 from random import randint
-
+ 
 def numar(x,stack):
      stack.append(x); #O(n)
  
 def p(arr,stack):
      arr.append(stack.pop()); #O(n)
-
+ 
 class Problem3(Problem):
     def __init__(self):
         statement = '3. Primiti o stiva. Operatii: \n'
@@ -15,29 +15,29 @@ class Problem3(Problem):
         statement += 'P -> se extrage un numar din stiva si se afiseaza \n'
         statement += 'Gasiti o succesiune de mutari a.i. introducand el. 1 2 3 4 5 in stiva (in aceasta ordine) la final sa se afiseze 3 2 4 5 1.'
         statement += '\n\n\n'
-        
+       
         data=[];
         n=randint(3, 99);  
         for i in range (1, n):
           data.append(randint(1, 99));
         super().__init__(statement, data)
-        
+       
     def solve(self):
           solution = 'Vom parcurge vectorul\n'
-          n = len(self.data);       
+          n = len(self.data);      
           stack = [];
           arr = [];
           ok = 0; #ok := verifica daca s-a citit pana la k (k actioneaza ca un separator)
           i = 0;
           contor = 1;
           n = n-1;
-          
+         
           if n <= 3:
-               k=radint(1,3);
+               k=randint(1,3);
           else:
-               k=radint(int(n/2), n-3);
+               k=randint(int(n/2), n-3);
                
-          while i <= n: 
+          while i <= n:
                       if i <= k:
                             if ok == 0:
                                    e = self.data[i]
@@ -55,8 +55,8 @@ class Problem3(Problem):
                                     p(arr,stack);
                                     i = i + 1;
                                     contor -= 1
-                                        
-                            if ok == 1 & contor == 1: 
+                                       
+                            if ok == 1 & contor == 1:
                                     contor = k + 1;
                                    
                       else:
@@ -69,9 +69,9 @@ class Problem3(Problem):
                             p(arr,stack);
                             contor = contor + 1;
                             i = i + 1;
-                                
-          solution += 'Se va elimina din stiva numarul ' + str(stack[0]) + '\n'   
+                               
+          solution += 'Se va elimina din stiva numarul ' + str(stack[0]) + '\n'  
           p(arr,stack);
-          
+         
           solution += 'Rezultatul final este : ' + str(arr) + '\n'
           return solution
