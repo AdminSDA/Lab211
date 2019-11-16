c57e7e4c68459f7907b062b11b04f4f49d1d3c0c
Freeman-A <56113049+Freeman-A@users.noreply.github.com>
2019-11-16 17:40:43 +0200

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,16 +1,15 @@
 from problem import Problem
-import random
 from random import randint
  
 def numar(x,stack):
-     stack.append(x); #O(n)
+     stack.append(x) #O(n)
  
 def p(arr,stack):
-     arr.append(stack.pop()); #O(n)
+     arr.append(stack.pop()) #O(n)
      
 
 def p1(stack,x,vec):
-     vec.append(stack.pop(x));
+     vec.append(stack.pop(x))
  
 class Problem3(Problem):
     def __init__(self):
@@ -18,37 +17,38 @@ class Problem3(Problem):
         statement += 'numar -> se inseaza numarul in stiva \n'
         statement += 'P -> se extrage un numar din stiva si se afiseaza \n'
         statement += 'Introduceti in stiva urmatoarele numere: ' 
-        data=[];
-        n=randint(3, 99);  
+        data=[]
+        n=randint(3, 99)  
         for i in range (1, n):
-          data.append(randint(1, 99));
+          data.append(randint(1, 99))
         statement += 'si determinati operatiile pentru care se afiseaza: '
-        stack = data;
-        i=1;
-        n=len(stack);
+        temp = data
+        i=1
+        n=len(data)
         if n <= 3:
-               k=randint(1,3);
+               k=randint(1,3)
           else:
-               k=randint(int(n/2), n-3);
-        contor=e=k;
-        vec=[];
+               k=randint(int(n/3), n-3)
+        contor = k
+        vec=[]
         while i < n:
-                  p1(stack,contor,vec);
-                  if i < e:
-                      contor-=1;
+                  p1(temp,contor,vec)
+                  if i < k:
+                      contor = contor - 1
                   i = i + 1
-        p1(stack,contor-1,vec);
-        statement += str(vec);
+        contor = contor - 1      
+        p1(temp,contor,vec)
+        statement += str(vec)
         statement += "\n"
         super().__init__(statement, data)
        
     def solve(self):
           solution = 'Vom parcurge vectorul\n'
-          n = len(self.data) - 1; 
-          stack = [];
-          arr = [];
-          i=ok=0;
-          contor = 1;
+          n = len(self.data) - 1
+          stack = []
+          arr = []
+          i=ok=0
+          contor = 1
           k=self.k
           solution +='Se va folosi raportul ' + str(int(k/n*100)) + '% si ' + str(int((n-k)/n*100)) + '% \n'
           solution +='Operatiile folosite sunt: \n'
@@ -59,33 +59,33 @@ class Problem3(Problem):
                                    numar(e,stack)
                                    solution += 'numar(' + str(e) + ')\n'
                                    if i == k:
-                                              ok = 1;
-                                              i = 1;
+                                              ok = 1
+                                              i = 1
                                    else:
-                                        i = i + 1;
-                                        contor += 1;
+                                        i = i + 1
+                                        contor = contor + 1
                                        
                             elif contor-i != 0:
                                     solution += 'p()\n'
-                                    p(arr,stack);
-                                    i = i + 1;
-                                    contor = contor - 1;
+                                    p(arr,stack)
+                                    i = i + 1
+                                    contor = contor - 1
                                        
                             if ok == 1 & contor == 1:
-                                    contor = k + 1;
+                                    contor = k + 1
                                    
                       else:
                             if contor == n:
-                                break;
-                            e = self.data[contor];
-                            numar(e,stack);
+                                break
+                            e = self.data[contor]
+                            numar(e,stack)
                             solution += 'numar('+str(e)+')\n'
                             solution += 'p()\n'
                            
-                            p(arr,stack);
+                            p(arr,stack)
                             contor = contor + 1;
-                            i = i + 1;
+                            i = i + 1
                                
           solution += 'p()\n'   
-          p(arr,stack);
+          p(arr,stack)
           return solution
