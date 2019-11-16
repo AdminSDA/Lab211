fa3bc0808c12a1bb80cd1f6584b1d7d6c3155d2d
AndaTot <37780306+AndaTot@users.noreply.github.com>
2019-11-16 12:15:23 +0200

    Update problem11.py

--- problem11.py
+++ problem11.py
@@ -1,14 +1,28 @@
 import random
-from random import sample
+from random import randint
 from problem import Problem
 
 
 
 class Problem11(Problem):
     def __init__(self):
-        v = sample(range(1, 100), 10)
+        pivot=randint(1,99)
+        v=[0]
+        i=0
+        while(i<8):
+            j=randint(1,2)
+            aux=i
+            while(i<=aux+j):
+                v.append(randint(pivot+1,100))
+                i+=1
+            aux=i
+            while(i<=aux+j):
+                v.append(randint(1,pivot-1))
+                i+=1
+        i=randint(0,8)
+        v[i]=pivot
+
         statement = 'Partiționați următorul vector folosind pivotul '
-        pivot = random.choice(v)
         statement += str(pivot)
         statement += ' și partiționarea Lomuto: '
         statement += str(v)
@@ -28,7 +42,6 @@ class Problem11(Problem):
         data = self.data
         n = len(data)
         v = data
-
         solution='O partitionare a vectorului este:\n'
         solution+='inlocuieste-ma'+'\n'+'\n'+'\n'
         'string-ul de mai sus va fi inlocuit dupa partitionare cu vectorul sortat'
@@ -43,14 +56,12 @@ class Problem11(Problem):
         solution+='Vom calcula pozitia unde trebuie sa stea pivotul,crescand indicele si efectuand interschimbari a.i. elementele mai mici sau egale sa fie in stanga sa.\n'
         solution+='La sfarsit pivotul va fi mutat pe pozitia corespunzatoare.\n'
         solution+='Pozitia pivotului este initial -1 dar va deveni cel putin 0 pe parcursul algoritmului.\n'+'\n'
-
         pozpiv = -1
         j = 0
-
         while j < n:
             solution += 'Pasul ' + str(j + 1) + ' : '
-
             if v[j] <= piv:
+
                 pozpiv = pozpiv + 1
                 if(v[j]<piv): solution += 'Elementul ' + str(v[j]) + ' este mai mic decat pivotul'
                 else: solution+='Elementul '+str(v[j])+' este egal cu pivotul'
@@ -59,14 +70,14 @@ class Problem11(Problem):
                 v[pozpiv] = v[j]
                 v[j] = aux
                 j =j + 1
-
             else:
                 solution += 'Elementul ' + str(v[j]) + ' este mai mare decat pivotul ---> vectorul si pozitia pivotului raman aceleasi\n'
                 j = j + 1
-
-            solution+='Vectorul la acest pas al algoritmului este : '+str(v)+'\n'+'\n'
-
+            solution+='Vectorul la aces pas al algoritmului este : '+str(v)+'\n'+'\n'
+        if pozpiv >= n:
+            pozpiv += -1
         solution += '\n'
         solution=solution.replace('inlocuieste-ma',str(v),1)
         return solution
+
 
