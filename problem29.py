c8c0bb6a7857db060fed5be2c39de6dc66420c3c
Dima Darius <24Arys11@users.noreply.github.com>
2019-11-30 15:03:09 +0200

    problem9.py

--- problem29.py
+++ problem29.py
@@ -1,150 +1,59 @@
 from problem import Problem
 import random
+import math
 
-def find_zeros(array):
-     i=0
-     while i < len(array):
-        if array[i] == 0:
-               return 0
-        i = i + 1                      
-         
-def search_arr(vector, x):
-     i = 0
-     while i < len(vector):
-            if vector[i] == x:
-                return i;
-           
-            i += 1
-               
-     return 'error';
-     
-                   
-def knumar(vector, stanga, dreapta, k, solution, pas):
-        lungimea = dreapta - stanga + 1;
-        if k > 0 and k <= lungimea:
-                if pas == 1:
-                    solution += 'Pasul ' + str(stanga) + ' din ' + str(dreapta) + ': \n'
-                mediana_part_cinci = []
-                i = 0
-                while i < lungimea: #T(12/5*n) ~ O(n)
-                    e = lungimea - i
-                    if e > 5:
-                        e = 5
-                    mediana = []
-                    j = 0
-                    while j < e: #(j< 1, 2, 3, 4, 5) #T(5)
-                          mediana.append(vector[i + j + stanga])
-                          j += 1
-                    mediana.sort()  #T(5log5) ~ T(6)
-                    # per total ~ T(12) => ~ T(12) ~ O(1)
-                    mediana_part_cinci.append(mediana[e // 2])
-                    i += 5
-         
-                i = i // 5
-                med = knumar(mediana_part_cinci, 0, i - 1, i // 2, solution, 0) #O(1)
-                if i == 1:
-                    med = mediana_part_cinci[0] 
-                contor = verificare(vector, stanga, dreapta, med) #returneaza si ordoneaza vectorul in ordinea k
-                contor1 = contor - stanga
-                if pas == 1:
-                     solution += 'Numarul ' + str(med) + ' este al ' + str(contor1) + ' lea numar cel mai mic numar din sir '    
-                if (contor1 == k - 1):  
-                    return vector[contor]  
-                if (contor1 > k - 1):
-                    return knumar(vector, stanga, contor - 1, k, solution, 1)  
- 
-                return knumar(vector, contor + 1, dreapta, k - contor1 - 1, solution, 1)  
- 
- 
- 
-def swap(vector, a, b):  
-    temp = vector[a]  
-    vector[a] = vector[b]  
-    vector[b] = temp  
- 
- 
-def verificare(vector, start, sfarsit, numarul):
-    for i in range(start, sfarsit):
-        if vector[i] == numarul:
-            swap(vector, sfarsit, i)
-            break
- 
-    numarul = vector[sfarsit]
-    i = start  
-    for j in range(start, sfarsit):  #T(r - l)
-        if vector[j] <= numarul:
-           swap(vector, i, j)  
-           i += 1
-       
-    swap(vector, i, sfarsit)  
-    return i
-    
 class Problem29(Problem):
     def __init__(self):
-        k = random.randint(6,10)
-        k1 = k * 2
-        data = random.sample(range(100), k1);
- 
-        statement = '29. Se primesc numerele: ' + ', '.join(map(str, data)) + '. '
-        statement += 'Introduceti primele ' + str(k) + ' elemente in doi vectori ' + '\n'
-        statement += 'Inseram intr-un vector v1 primele elemente mai mari decat 0\n'
-        statement += 'daca primim un zero inainte, inseamna ca nu avem k elemente, deci le afisam pe toate.'
-        statement += 'Acum in acel vector v1 avem k elemente.'
-        statement += '\nContinuam sa citim urmatoarele k elemente intr-un vector v2.\n'      
-        statement += 'Cand primim 0 sau si v2 contine k elemente, punem la gramada elemente lui v1 si ale lui v2 '
-        statement += ' si aplicam statistici de ordine sa obtinem cele mai mici k elemente din v1 si v2.'
-        self.data = data
-        self.k = k
+        # Lungimea vectorului este intre 6-15 elemente (1-100)
+        lungime = random.randint(6, 15)
+
+        sir = random.sample(range(1, 100), lungime)
+        # Adaugam 0 la final
+        sir[lungime - 1] = 0
+        # Alegem un random k (2 - 5)
+        k = random.randint(2, 5)
+        # Suprascriem 0 peste cateva numere din vector de la sir[k] la sir[ultim]
+        zeroes = random.randint(1, math.floor((lungime - k) / 2 + 1))   # cand k = lungime ramane doar zeroul de la final.
+                                                            # cand lungimea e mare si k e mic, avem mai multe zerouri,
+                                                            # iar cand lungimea e mica si k e mare, avem mai putine.
+
+        for i in range(0, zeroes):
+            index = random.randint(k, lungime)
+            if index < (lungime - 1):  # sir[lungime - 1] este deja 0 !
+                if (sir[index + 1] != 0) and (sir[index - 1] != 0):  # Nu dorim sa avem doua zerouri alaturate
+                    sir[index] = 0
+
+        # Generam un vector x de k elemente in care copiem primele k elemente din vector.
+        x = sir[0:k]
+        # Sortam x
+        x.sort()
+
+        n = [lungime, k]
+
+        data = [sir, x, n]
+        # data = [v[v[0]], lungime[v[1]], x[v[2]], k[v[3]]]
+
+        statement = 'Primiti numere naturale > 0 si atunci cand primiti 0, trebuie sa afisati cele mai mari k elemente.\n '
+        statement += 'Se dau numerele: ' + ', '.join(map(str, sir)) + '.\n k = ' + str(k) + '\n'
         super().__init__(statement, data)
- 
+
+
     def solve(self):
-        solution = 'Implementam primele ' + str(self.k) + ' numere in vectorul v1 \n'
-        i = 0
-        temp = []
-        while i < self.k:
-            temp.append(self.data[i])
-            i = i + 1
-       
-        solution += 'Cautam zerouri daca apar in primul vector: \n'    
-        if find_zeros(temp) == 0:
-             solution += 'Au aparut zerouri in v1 deci se va afisa: \n'
-             i = 0
-             final = []
-             save = 0
-             while i < len(self.data):
-                 if self.data[i] != 0:
-                     save = i
-                     final.append(self.data[i])
-                 
-                 i = i + 1
-             solution += 'Primele ' + str(save) + ' numere sunt: \n'
-             solution += str(final)
- 
-        else:
-                solution += 'Nu au aparut zerouri in v1 deci continuam... \n'    
-                i = self.k
-                j = i * 2
-                solution += 'Importam numere in vectorul v2: \n'
-                while i < j:
-                    if self.data[i] == 0:
-                          break
-                    temp.append(self.data[i])
-                    i = i + 1
-                e = knumar(temp, 0, len(temp) - 1, self.k, solution, 1)
-                if self.k == 1:
-                    solution += 'Primul numar este: ' + str(e) + '\n'
-                else:
-                    solution += 'Numarul al ' + str(self.k) + ' lea cel mai mic element este ' + str(e) + '\n'
-                solution += 'Se cauta numere mai mici decat numarul: ' + str(self.k) + ' si se ordoneaza: \n'
-                final = []
-                i = 0
-                index = search_arr(temp, e)
-                while i <= index:
-                     final.append(temp[i])
-                     i = i + 1
-                solution += 'Ordonare completa. Primele ' + str(self.k) + ' elemente sunt: \n'
-                solution += str(final);
-                final.sort();
-                solution += '\nNumerele sortate sunt: ' + str(final); 
-                
+
+        solution = '\n'
+        for i in range(self.data[2][1], self.data[2][0]):
+            if self.data[0][i] == 0:
+                # afisam x
+                solution += ', '.join(map(str, self.data[1])) + '\n'
+            elif self.data[0][i] < self.data[1][self.data[2][1] - 1]:
+                self.data[1][self.data[2][1] - 1] = self.data[0][i]
+                # ducem x[k - 1] pe pozitia potrivita si vectorul x va fi din nou sortat !
+                for j in range(self.data[2][1] - 1, 0, -1):
+                    if self.data[1][j] < self.data[1][j - 1]:
+                        aux = self.data[1][j]
+                        self.data[1][j] = self.data[1][j - 1]
+                        self.data[1][j - 1] = aux
+                    else:
+                        break # x[j] e pe pozitia care trebuie ! Nu este necesar sa verificam restul elementelor, intrucat acestea au fost sortate anterior !
+
         return solution
