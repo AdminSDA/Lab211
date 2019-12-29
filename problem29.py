5ed1db34eed90f6e12349419721da19a48f91e4f
Andreas Mihalea <56113049+Freeman-A@users.noreply.github.com>
2019-12-29 19:16:29 +0200

    Update problem29.py

--- problem29.py
+++ problem29.py
@@ -2,58 +2,78 @@ from problem import Problem
 import random
 import math
 
+def heap(vector, n, i, stanga, dreapta):
+    max = i;
+    if stanga < n and vector[stanga] > vector[max]:
+        max = stanga;
+    if dreapta < n and vector[dreapta] > vector[max]:
+        max = dreapta 
+    if max != i:
+       vector[i], vector[max] = vector[max], vector[i]
+       heap(vector, n, max, 2*max + 1, 2*max + 2)
+   
+    
+def algorithm(vector, k, temp): # k - cel mai mic element 
+    n = len(vector)
+    i = int(n / 2) - 1
+    while i >= 0:             
+        heap(vector, n, i, 2*i+1, 2*i+2) # scriem in heap
+        i = i - 1
+        
+    i = n - 1
+    
+    while i >= 0:
+        vector[0], vector[i] = vector[i], vector[0]
+        heap(vector, i, 0, 1, 2)    
+        i = i - 1 
+    
+    eliminare(vector, k, temp)
+    
+def eliminare(vector, k, temp):
+    i = 0
+    while i < k:
+        temp.append(vector[i]);
+        i = i + 1;
+    
+    
+def despartire(vecmax, k, solution):
+    e = len(vecmax);
+    i = 0;
+    counter = 0;
+    vector = [];
+    while i < e:
+        if len(vector) >= k and vecmax[i] == 0:
+                        solution += 'Index ' + str(counter) + '\nVectorul este:\n'
+                        solution += str(vector) + '\nDupa algoritm vectorul devine: \n'
+                        temp = [];
+                        algorithm(vector, k, temp)
+                        solution += temp + '\n'
+                        vector = temp;
+                        counter = counter + 1;
+        
+        if vecmax[i] != 0:
+            vector.append(vecmax[i]);
+        i = i + 1;
+             
 class Problem29(Problem):
     def __init__(self):
         # Lungimea vectorului este intre 6-15 elemente (1-100)
-        lungime = random.randint(6, 15)
-
+        lungime = random.randint(6, 30)
+        k = random.randint(1, lungime);
         sir = random.sample(range(1, 100), lungime)
         # Adaugam 0 la final
         sir[lungime - 1] = 0
-        # Alegem un random k (2 - 5)
-        k = random.randint(2, 5)
-        # Suprascriem 0 peste cateva numere din vector de la sir[k] la sir[ultim]
-        zeroes = random.randint(1, math.floor((lungime - k) / 2 + 1))   # cand k = lungime ramane doar zeroul de la final.
-                                                            # cand lungimea e mare si k e mic, avem mai multe zerouri,
-                                                            # iar cand lungimea e mica si k e mare, avem mai putine.
-
-        for i in range(0, zeroes):
-            index = random.randint(k, lungime)
-            if index < (lungime - 1):  # sir[lungime - 1] este deja 0 !
-                if (sir[index + 1] != 0) and (sir[index - 1] != 0):  # Nu dorim sa avem doua zerouri alaturate
-                    sir[index] = 0
-
-        # Generam un vector x de k elemente in care copiem primele k elemente din vector.
-        x = sir[0:k]
-        # Sortam x
-        x.sort()
-
-        n = [lungime, k]
-
-        data = [sir, x, n]
-        # data = [v[v[0]], lungime[v[1]], x[v[2]], k[v[3]]]
-
+        nr1 = random.randint(1, lungime);
+        nr2 = random.randint(1, lungime);
+        nr3 = random.randint(1, lungime);
+        sir[nr1] = 0
+        sir[nr2] = 0
+        sir[nr3] = 0
         statement = 'Primiti numere naturale > 0 si atunci cand primiti 0, trebuie sa afisati cele mai mari k elemente.\n '
         statement += 'Se dau numerele: ' + ', '.join(map(str, sir)) + '.\n k = ' + str(k) + '\n'
         super().__init__(statement, data)
 
-
     def solve(self):
-
-        solution = '\n'
-        for i in range(self.data[2][1], self.data[2][0]):
-            if self.data[0][i] == 0:
-                # afisam x
-                solution += ', '.join(map(str, self.data[1])) + '\n'
-            elif self.data[0][i] < self.data[1][self.data[2][1] - 1]:
-                self.data[1][self.data[2][1] - 1] = self.data[0][i]
-                # ducem x[k - 1] pe pozitia potrivita si vectorul x va fi din nou sortat !
-                for j in range(self.data[2][1] - 1, 0, -1):
-                    if self.data[1][j] < self.data[1][j - 1]:
-                        aux = self.data[1][j]
-                        self.data[1][j] = self.data[1][j - 1]
-                        self.data[1][j - 1] = aux
-                    else:
-                        break # x[j] e pe pozitia care trebuie ! Nu este necesar sa verificam restul elementelor, intrucat acestea au fost sortate anterior !
-
+        solution=""
+        despartire(self.sir, self.k, solution)
         return solution
