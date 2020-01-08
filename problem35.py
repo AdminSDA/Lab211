bd59fd2b7e5f8b4b1c29b04bad38884f1055f89d
Anca260 <56958311+Anca260@users.noreply.github.com>
2020-01-08 23:47:04 +0200

    Add files via upload

---
+++ problem35.py
@@ -0,0 +1,133 @@
+import random
+from random import sample
+from problem import Problem
+
+
+class Fifo():
+    def __init__(self, val):
+        self.val = val
+        self.st = None
+        self.dr = None
+        self.h = 0
+
+
+class Problem35(Problem):
+
+    def __init__(self):
+        self.statement = 'Inserati pe rand urmatoarele numere in AVL: '
+        self.data = []
+        n = random.randint(5, 15)
+        v = random.sample(range(25), n)
+
+        self.data = [n, v]
+        self.statement += str(self.data[1])
+
+        super().__init__(self.statement, self.data)
+
+    def inserare(self, rad, nod):
+        if rad.val < nod.val:
+            if rad.dr is None:
+                rad.dr = nod
+            else:
+                self.inserare(rad.dr, nod)
+        else:
+            if rad.st is None:
+                rad.st = nod
+            else:
+                self.inserare(rad.st, nod)
+
+    def preorder(self, rad, w):
+        if rad:
+            w.append(rad.val)
+            self.preorder(rad.st, w)
+            self.preorder(rad.dr, w)
+
+    def fct(self, nod):
+        if nod is None:
+            return -1
+        return nod.h
+
+
+    def functie_h(self, nod):
+        return max(self.fct(nod.st), self.fct(nod.dr)) + 1
+
+
+    def factor_ec(self, nod):
+        if nod is None:
+            return 0
+        return self.fct(nod.st) - self.fct(nod.dr)
+
+
+    def SRL(self, nod):
+        rightNode = nod.dr
+        nod.dr = rightNode.st
+        rightNode.st = nod
+
+        nod.h = self.functie_h(nod)
+        rightNode.h = self.functie_h(rightNode)
+        return rightNode
+
+    def SRR(self, nod):
+        leftNode = nod.st
+        nod.st = leftNode.dr
+        leftNode.dr = nod
+
+        nod.h = self.functie_h(nod)
+        leftNode.h = self.functie_h(leftNode)
+        return leftNode
+
+    def DRL(self, nod):
+        nod.dr = self.SRR(nod.dr)
+        nod = self.SRL(nod)
+        return nod
+
+    def DRR(self, nod):
+        nod.st = self.SRL(nod.st)
+        nod = self.SRR(nod)
+        return nod
+
+    def inserare_avl(self, act, value):
+        if act is None:
+            return Fifo(value)
+        elif value < act.val:
+            act.st = self.inserare_avl(act.st, value)
+        else:
+            act.dr = self.inserare_avl(act.dr, value)
+
+        act.h = self.functie_h(act)
+        fe = self.factor_ec(act)
+
+        if fe == -2:
+            if value > act.dr.val:
+                act = self.SRL(act)
+            else:
+                act = self.DRL(act)
+
+        elif fe == 2:
+            if value < act.st.val:
+                act = self.SRR(act)
+            else:
+                act = self.DRR(act)
+
+        return act
+
+    def solve(self):
+        solution = ''
+
+        n = self.data[0]
+        v = self.data[1]
+
+        rad = None
+
+        for i in range(0, n):
+            rad = self.inserare_avl(rad, v[i])
+            solution += 'AVL-ul dupa inserarea elementului ' + str(v[i]) + ' este: '
+            w = []
+            self.preorder(rad, w)
+            for i in range(0, len(w)):
+                solution += str(w[i])
+                solution += ' '
+            solution += '\n'
+
+        return solution
+
