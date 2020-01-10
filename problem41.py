6bf956c0ebfd3ef5bef567918f84e70691c805c5
DiaconuAndr3i <57724130+DiaconuAndr3i@users.noreply.github.com>
2020-01-10 11:29:58 +0200

    Update problem41.py

--- problem41.py
+++ problem41.py
@@ -16,20 +16,20 @@ class Problem41(Problem):
         a = random.randint(3, 8) #nr de noduri
         noduri = random.sample(range(1,a+1), a) #vector cu etichetele nodurilor de la 1 la a+1
         data = []
-        data.append(graph(noduri[0],noduri[1],random.randint(1,50))) #vector care contine initial prima muchie cu nod1, nod2, cost
+        data.append(graph(noduri[0],noduri[1],random.randint(1,20))) #vector care contine initial prima muchie cu nod1, nod2, cost
         for i in range(2,a):
-            data.append(graph(noduri[random.randint(0,i-1)],noduri[i],random.randint(1,50))) #vectorul va contine a-1 muchii, reprezentand un arbore initial
-        m = random.randint(0,math.floor(((a-1)*(a-2))/2)) #nr de muchii care mai pot fi adaugate in graf
+            data.append(graph(noduri[random.randint(0,i-1)],noduri[i],random.randint(1,20))) #vectorul va contine a-1 muchii, reprezentand un arbore initial
+        m = random.randint(0,math.floor(((a-1)*(a-2))/4)) #nr de muchii care mai pot fi adaugate in graf
         for i in range (0,m):
             while True:
                 ok = False
                 u = random.sample(range(1, a+1), random.randint(2, 2)) # genereaza 2 noduri
-                muchie = graph(u[0],u[1], random.randint(1, 50)) # formeaza muchia din nodurile de mai sus
+                muchie = graph(u[0],u[1], random.randint(1, 20)) # formeaza muchia din nodurile de mai sus
                 for j in data:
                     if ((muchie.nod1 == j.nod1) and (muchie.nod2 == j.nod2) or (muchie.nod1 == j.nod2) and (muchie.nod2 == j.nod1)): #verifica daca muchia se afla in lista de muchii deja
                         ok = True
                         break
-                if ok == False: #daca muchia nu se afla in lista de muchii deja, se adauga
+                if ok == False: #daca muchia nu se afla in lista de muchii deja se adauga
                     break
             data.append(graph(muchie.nod1,muchie.nod2,muchie.cost))
 
@@ -43,13 +43,14 @@ class Problem41(Problem):
         super().__init__(statement, data)
 
     def solve(self):
-        solution = 'Arborele obtinut este: \n'
+
         v = self.data[0] #lista de muchii
         nr_n = self.data[1] #nr de noduri
         st = v[0].nod1 #nodul de start
         culoare = [] #vector de culoare
         hmin = []
-
+        solution ='Nodul de start este: ' + str(st) + '\n'
+        solution += 'Arborele obtinut este: \n'
         la = []
         for i in range(0,nr_n):
             w = []
